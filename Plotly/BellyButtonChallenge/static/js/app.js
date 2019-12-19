869ab719cd8df24999de6c039dd6f45be36da3cb
hmars333 <52890267+hmars333@users.noreply.github.com>
2019-12-19 10:49:22 -0800

    update app.js

--- Plotly/BellyButtonChallenge/static/js/app.js
+++ Plotly/BellyButtonChallenge/static/js/app.js
@@ -22,20 +22,72 @@ function charts(sample){
     var otu_ids= result.otu_ids;
     var otu_labels= result.otu_labels;
     var sample_values= result.sample_values;
-    
-  })
+
+
+    var bubbleLayout= {
+      title: "Bacteria Per Sample",
+      margin: {t: 50},
+      hovermode: "closest",
+      xaxis: { title: "OTU ID" },
+      margin: {t: 50},
+    };
+
+    var bubbleData = [
+      {
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: "markers",
+        marker: {
+          size: sample_values,
+          color: otu_ids,
+          colorscale: "Earth"
+        }
+
+    }
+  ];
+Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+var yticks= otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse();
+var barChart=[
+  {
+    y: yticks,
+    x: sample_values.slice(0,10).reverse(),
+    text: otu_labels.slice(0, 10).reverse(),
+    type: "bar",
+    orientation: "h",   
+  }
+];
+
+var layout= {
+  title: "Top 10 Most Common Bacteria",
+  margin: { l: 50, r: 50}
+};
+
+Plotly.newPlot("bar", barChart, layout);
+  });
 }
 
+function init() {
+  var selector = d3.select("#selDataset"); 
+  
+  d3.json("../samples.json").then((sampleData) => {
+    var sampleNames = sampleData.names;
+    sampleNames.forEach((sample) => {
+      selector.append("option")
+      .text(sample)
+      .property("value", sample);
+    });
 
-// var oTU= samples.samples.otu_ids;
-// console.log(oTU);
-// var otuValues= samples.samples.sample_values;
-// console.log(otuValues);
-// var otuIndiv= samples.names;
-// console.log(otuIndiv);
+    var firstSample= sampleNames[0];
+    buildCharts(firstSample);
+    buildData(firstSample);  
+  });
+}
 
+function optionChange(newSample) {
+  buildCharts(newSample);
+  buildData(newSample);
+}
 
-// sampleData.forEach(function(data){
-//   return sampleData.slice(0,11);
-// })
-
+init();
