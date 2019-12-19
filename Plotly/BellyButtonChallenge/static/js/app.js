a7401553dc23a83dcca47c2b30810b2792ec160e
hmars333 <52890267+hmars333@users.noreply.github.com>
2019-12-19 09:43:29 -0800

    updates

--- Plotly/BellyButtonChallenge/static/js/app.js
+++ Plotly/BellyButtonChallenge/static/js/app.js
@@ -1,6 +1,30 @@
-var sample= d3.json("../samples.json", function(data){
-  console.log(sample);
-})
+function data(sample){
+  d3.json("../samples.json").then((sampleData) => {
+    var metaData= sampleData.metadata;
+    var results= metaData.filter(sampleObj.id == sample);
+    var result = results[0];
+    var panel= d3.select("#sample-metadata");
+  
+    Object.entries(result).forEach(([key, value]) => {
+      panel.append("h6").text(`${key.toUppercase()}: ${value}`);
+    });
+  
+  });  
+  
+}
+
+function charts(sample){
+
+  d3.json("../samples.json").then((sampleData) => {
+    var samples= sampleData.samples;
+    var results= samples.filter(sampleObj => sampleObj.id== sample);
+    var result =results[0];
+    var otu_ids= result.otu_ids;
+    var otu_labels= result.otu_labels;
+    var sample_values= result.sample_values;
+    
+  })
+}
 
 
 // var oTU= samples.samples.otu_ids;
